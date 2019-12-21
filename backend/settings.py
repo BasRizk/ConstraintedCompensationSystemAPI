4269d057358133107a6fa0159bd9d89e3b3558eb
unknown <ibram.medhat2@gmail.com>
2019-12-22 00:13:33 +0200

    Editing settings to allow access from another domain

--- backend/settings.py
+++ backend/settings.py
@@ -33,7 +33,10 @@ REST_FRAMEWORK = {
 
 # Application definition
 
+CORS_ORIGIN_ALLOW_ALL=True
+
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +48,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
