0835238f99fb199c2867a120adf4f45f6ff8a34f
Steven Henry <stevennassef97@gmail.com>
2019-12-25 14:33:08 +0200

    1- Updated Models

--- api/models.py
+++ api/models.py
@@ -37,3 +37,38 @@ class Slot(models.Model):
     def __unicode__(self):
         return u"Slot at time %s in location %s" % (str(
             self.slot_num), str(self.slot_location))
+
+class CompensatedSlot(models.Model):
+    """
+    DB Model representing a single slot in the compensation schedule
+    """
+    id = models.AutoField(primary_key=True)
+    slot_date = models.DateField()
+    slot_id = models.IntegerField()
+    slot_num = models.IntegerField(validators=SLOT_NUM_VALIDATORS)
+    slot_subject = models.CharField(max_length=60)
+    slot_type = models.CharField(max_length=32)
+    slot_group = models.CharField(max_length=60)
+    slot_subgroup = models.CharField(max_length=60)
+    slot_location = models.IntegerField(validators=SLOT_LOCATION_VALIDATORS)
+    slot_teacher = models.CharField(max_length=60)
+
+    def __str__(self):
+        return '{}, {}, {}, {}, {}, {}, {}, {}, {}'.format(
+                                            self.slot_date,
+                                            self.slot_id,
+                                            self.slot_num,
+                                            self.slot_subject,
+                                            self.slot_type,
+                                            self.slot_group,
+                                            self.slot_subgroup,
+                                            self.slot_location,
+                                            self.slot_teacher)
+
+    class Meta:
+        verbose_name_plural = 'slots'
+        ordering = ['slot_date', 'slot_num']
+
+    def __unicode__(self):
+        return u"Slot at time %s in location %s" % (str(
+            self.slot_num), str(self.slot_location))
